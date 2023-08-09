e711fa8557a38d845a835b2791ec98e92ab8b3f0
hyohyo <sojaisam@gmail.com>
2023-08-09 16:50:56 -0700

    Profile page with ssg helper

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,5 @@
 import { clerkClient } from "@clerk/nextjs";
-import { type User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
-import { privateDecrypt } from "crypto";
 import { z } from "zod";
 import {
   createTRPCRouter,
@@ -10,14 +8,9 @@ import {
 } from "~/server/api/trpc";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profilePicture: user.imageUrl,
-  };
-};
+
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
