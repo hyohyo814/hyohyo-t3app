c7f4b6965bd197c9149387b547165f521be85d8f
hyohyo <sojaisam@gmail.com>
2023-08-09 11:50:14 -0700

    working posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,13 @@
 import { clerkClient } from "@clerk/nextjs";
 import { type User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
+import { privateDecrypt } from "crypto";
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -16,6 +21,7 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: [{ createdAt: "desc" }],
     });
 
     const users = (
@@ -28,12 +34,12 @@ export const postsRouter = createTRPCRouter({
     console.log(users);
 
     return posts.map((post) => {
-      const author = users.find((user) => user.id === post.authorId)
+      const author = users.find((user) => user.id === post.authorId);
 
       if (!author?.username)
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
-          message: "Author for post not found"
+          message: "Author for post not found",
         });
 
       return {
@@ -45,4 +51,23 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
