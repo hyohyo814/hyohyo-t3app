086fbc3bdf631feade673039fd970587562bf714
hyohyo <sojaisam@gmail.com>
2023-08-09 17:11:08 -0700

    render posts on profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -10,6 +10,22 @@ import { appRouter } from "~/server/api/root";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
+import { PostView } from "~/components/postview";
+
+const ProfileFeed = (props: { userId: string }) => {
+  const {data, isLoading} = api.posts.getPostsByUserId.useQuery({userId: props.userId});
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length === 0) return <div>User has not posted</div>;
+
+  return (
+    <div className="flex flex-col">
+      {data.map((fullPost) => (<PostView {...fullPost} key={fullPost.post.id}/>))}
+    </div>
+  )
+}
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({ username });
@@ -32,8 +48,9 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
           />
         </div>
         <div className="h-[64px]"></div>
-        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
-        <div className="border-b border-slate-400 w-full"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
+        <div className="border-b border-slate-400 w-full" />
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { NextPage } from "next";
 import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
 
 dayjs.extend(relativeTime);
 
@@ -74,37 +75,6 @@ const CreatePostWizard = () => {
           <LoadingSpinner size={20} />
         </div>
       )}
-    </div>
-  );
-};
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-
-  return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        src={author.profilePicture}
-        alt={`@${author.username}'s picture`}
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col">
-        <div className="flex whitespace-pre text-slate-300">
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username}`}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">
-              {` · ${dayjs(post.createdAt).fromNow()}`}
-            </span>
-          </Link>
-        </div>
-        <span className="text-xl">{post.content}</span>
-      </div>
     </div>
   );
 };
